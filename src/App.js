b82bf8d5564ea758ee1273459fa4bda2f5b239d5
Fachrur Rizhky Al Fadillah <122960680+fahruulc@users.noreply.github.com>
2023-05-21 12:14:06 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Example from "./components/Example/Example";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Example />
+    </>
+  );
+}
+
+export default App;
